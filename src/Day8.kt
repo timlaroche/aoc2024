0a3ab1afce20737fe36510716d9c8876136b71ec
Tim La Roche <5930391+timlaroche@users.noreply.github.com>
2024-12-08 16:39:42 +0000

    day8+hangover - 2 days (7+8) in one day

---
+++ src/Day8.kt
@@ -0,0 +1,93 @@
+import kotlin.math.abs
+
+class Day8(isTest: Boolean) : Day(isTest) {
+    // 321 too high
+    fun part1(): Long {
+        val lines = Helper.readAsLines(inputFile)
+        val rows = lines.size
+        val columns = lines.first().toCharArray().size
+        val grid = Array(rows) { Array(columns) { '!' } }
+        var total = 0L
+
+        // Setup Grid
+        val frequencies = mutableMapOf<Char, Int>()
+        lines.forEachIndexed { i, line ->
+            line.toCharArray().forEachIndexed { j, c ->
+                if (!c.equals('.')) frequencies.compute(c, { _, v -> v?.plus(1) ?: 1 })
+                grid[i][j] = c
+            }
+        }
+
+        val antinodes = mutableSetOf<Pair<Int, Int>>()
+        frequencies.filterValues { x -> x > 1 }.forEach { freq ->
+            val antennas = scanGrid(grid, rows, columns, freq.key)
+
+            for ( i in 0 until antennas.size) {
+                for (j in 0 until antennas.size) {
+                    val antenna1 = antennas[i]
+                    val antenna2 = antennas[j]
+                    val rowDistance = antenna2.first - antenna1.first
+                    val columnDistance = antenna2.second - antenna1.second
+                    if (rowDistance > 0 || columnDistance > 0) {
+                        antinodes.add(Pair(antenna1.first - rowDistance, antenna1.second - columnDistance))
+                        antinodes.add(Pair(antenna2.first + rowDistance, antenna2.second + columnDistance))
+                    }
+                }
+            }
+            antinodes.removeIf{ it.first < 0 || it.first >= rows || it.second < 0 || it.second >= columns }
+        }
+        return antinodes.size.toLong()
+    }
+
+    fun scanGrid(grid: Array<Array<Char>>, rows: Int, columns: Int, frequency: Char): List<Pair<Int, Int>> {
+        val antennas = mutableListOf<Pair<Int, Int>>()
+        for (i in 0 until rows) {
+            for (j in 0 until columns) {
+                if(grid[i][j].equals(frequency)) antennas.add(Pair(i,j))
+            }
+        }
+        return antennas
+    }
+
+
+    // 984 too low
+    fun part2(): Long {
+        val lines = Helper.readAsLines(inputFile)
+        val rows = lines.size
+        val columns = lines.first().toCharArray().size
+        val grid = Array(rows) { Array(columns) { '!' } }
+        var total = 0L
+
+        // Setup Grid
+        val frequencies = mutableMapOf<Char, Int>()
+        lines.forEachIndexed { i, line ->
+            line.toCharArray().forEachIndexed { j, c ->
+                if (!c.equals('.')) frequencies.compute(c, { _, v -> v?.plus(1) ?: 1 })
+                grid[i][j] = c
+            }
+        }
+
+        val antinodes = mutableSetOf<Pair<Int, Int>>()
+        frequencies.filterValues { x -> x > 1 }.forEach { freq ->
+            val antennas = scanGrid(grid, rows, columns, freq.key)
+
+            for ( i in 0 until antennas.size) {
+                for (j in 0 until antennas.size) {
+                    val antenna1 = antennas[i]
+                    val antenna2 = antennas[j]
+                    val rowDistance = antenna2.first - antenna1.first
+                    val columnDistance = antenna2.second - antenna1.second
+                    if (rowDistance > 0 || columnDistance > 0) {
+                        // Major hack?
+                        for ( factor in -100..100) {
+                            antinodes.add(Pair(antenna1.first - (factor * rowDistance), antenna1.second - (factor * columnDistance)))
+                            antinodes.add(Pair(antenna2.first + (factor * rowDistance), antenna2.second + (factor * columnDistance)))
+                        }
+                    }
+                }
+            }
+            antinodes.removeIf{ it.first < 0 || it.first >= rows || it.second < 0 || it.second >= columns }
+        }
+        return antinodes.size.toLong()
+    }
+}
--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,5 @@
 fun main() {
-    val day = Day7(false)
+    val day = Day8(false)
     println(day.part1())
     println(day.part2())
 }
